7f7767f2b5d30f25117e7259312fbc342d5278c5
Oleg Nekrasov <79853886592@yandex.ru>
2022-05-23 16:09:02 +0300

    Create

---
+++ ListInsertPerformance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ListInsertPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // читаем весь файл с рабочего стола в строку текста
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TextFile1.txt");
+            string text = File.ReadAllText(path);
+            //string text = File.ReadAllText(@"E:\C#\Skillfactory\Учеба C#\Проекты\13\cdev_Text.txt");
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] {'\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            // выводим количество
+            Console.WriteLine(words.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
